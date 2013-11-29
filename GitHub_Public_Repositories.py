601c3ad6f7ab16e2274603056c7e6b2f8d2f853f
Max Woolf <max@minimaxir.com>
2013-11-28 21:13:36 -0800

    initial 2

---
+++ GitHub_Public_Repositories.py
@@ -0,0 +1,54 @@
+import urllib2
+import re
+import sys
+import json
+import csv
+from collections import defaultdict
+from random import random
+
+auth_token = '' # insert your Personal Auth Token here
+iterations = 3000
+
+req = urllib2.Request('https://api.github.com/repositories')
+req.add_header('Authorization', 'token %s' % auth_token)
+
+response = urllib2.urlopen(req)
+
+data = json.loads(response.read())
+
+repo_names = []
+repo_desc = []
+
+for i in range(iterations):
+    print i
+
+
+    for repo in data:
+        try:
+            repo_names.append(repo['name'].replace("-"," ").replace("_"," ").encode('utf-8').strip())
+            repo_desc.append(repo['description'].encode('utf-8').strip())
+        except:
+            pass
+    try:
+        req = urllib2.Request('https://api.github.com/repositories?since=%s' % data[len(data)-1]["id"])
+        req.add_header('Authorization', 'token %s' % auth_token)
+        response = urllib2.urlopen(req)
+        data = json.loads(response.read())
+    except:
+        pass
+
+print "done!"
+
+with open('github_repo_names.txt', 'w') as fp:
+    a = csv.writer(fp, delimiter='\n')
+    a.writerows([repo_names,])
+
+with open('github_repo_desc.txt', 'w') as fp:
+    a = csv.writer(fp, delimiter='\n')
+    a.writerows([repo_desc,])
+
+
+
+
+
+
--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 get-github-repo-descriptions
 ============================
 
-Gets the discriptions for a specified number of public GitHub repositories.
+Gets the discriptions for a specified number of public GitHub repositories. (and the repository names for good measure.) Uses the personal access token created in the GitHub admin.
+
+Retrieves 100 * number of iterations (3000 by default). The GitHub API limit is 5000 calls/hr, but the query & processing takes about 1 second each iteration, so there should be no rate limiting issues.
